71b758c0330858387ab6e0a8dd9f5948300e16c7
cartory <xXcartoryXx@gmail.com>
2021-10-09 08:03:52 -0400

    add table

---
+++ lib/models/table.ts
@@ -0,0 +1,29 @@
+export interface Table {
+	id: string
+	name: string
+	dataModel: string
+	columns: Column[]
+}
+
+export interface Column {
+	id: string
+	name: string
+	type: string
+	comment: string
+	generated: string
+
+	default: any
+	length: number
+
+	unique: boolean
+	primary: boolean
+	nullable: boolean
+
+	references: string
+}
+
+export interface TableFactory {
+	createModel(table: Table): string
+	createRoute(tableName: string): string
+	createController(tableName: string): string
+}
