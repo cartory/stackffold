d63946215cf08375ff6adf17dc58d43b26c482a3
cartory <xXcartoryXx@gmail.com>
2021-10-09 12:30:21 -0400

    add generator.ts

---
+++ lib/scripts/generator.ts
@@ -0,0 +1,32 @@
+import { Column, Table } from '../models/table'
+
+let database: Table[] = JSON.parse(
+	JSON.stringify(require('../../assets/database.json'))
+)
+
+const getForeignKeys = (columns: Column[]): string[] => { 
+	return columns
+		.filter(({ references }) => references)
+		.map(({ references }) => references)
+}
+
+const getReferencedTables = (tables: Table[], foreignKeys: string[]): Table[] => { 
+	return tables.filter(({ columns }) => { 
+		return columns.some(({ id }) => foreignKeys.indexOf(id) >= 0)
+	})
+}
+
+const generateFiles = (tables: Table[]) => { 
+	tables.forEach(table => { 
+		if (!table.marked) { 
+			table.marked = true
+			let foreignKeys: string[] = getForeignKeys(table.columns)
+			let referencedTables: Table[] = getReferencedTables(tables, foreignKeys)
+
+			generateFiles(referencedTables)
+			console.log(`${table.name} => GENERATED`);
+		}
+	})
+}
+
+generateFiles(database)
