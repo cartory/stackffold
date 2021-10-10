b633a957b7d8b4c2860c68bd1d22406ed51cc57f
cartory <xXcartoryXx@gmail.com>
2021-10-10 13:49:25 -0400

    update table > buildRoutes

--- lib/models/table.ts
+++ lib/models/table.ts
@@ -35,6 +35,7 @@ export interface Reference {
 export abstract class TableBuilder {
 	protected path: string
 	protected data: string
+	protected tableNames: string[] = []
 	
 	protected writeFile(): void {
 		fs.writeFile(this.path, this.data, {
@@ -46,7 +47,27 @@ export abstract class TableBuilder {
 		this.buildModel(table)
 		if (table.dataModel == 'Physical') { 
 			this.buildController(table)
+			this.tableNames.push(table.name)
 		}
+		
+		console.log(`${table.name} => GENERATED`);
+	}
+
+	public buildRoutes(): void {
+		this.tableNames.sort((a, b) => a.length - b.length)
+
+		this.data = (''
+			+ "import { Router } from 'express'\n"
+			+ "import { attachControllers } from '@decorators/express'\n\n"
+			+ `${this.tableNames.map(name => {
+				return `import { ${name}Controller } from './controllers/${name}Controller'`
+			}).join('\n')}\n\nconst router = Router()\n\n`
+			+ `attachControllers(router, [\n${this.tableNames.map(name => `\t${name}Controller,`).join('\n')}\n])\n\n`
+			+ 'export { router }'
+		)
+		this.path = 'src/routes.ts'
+
+		this.writeFile()
 	}
 
 	public abstract buildModel(table: Table): void
