fe3479d1e64825a5ae45c660b6eafba7b4a7b663
cartory <xXcartoryXx@gmail.com>
2021-12-22 15:11:57 -0400

    update generator, with sequelize

--- lib/scripts/generator.ts
+++ lib/scripts/generator.ts
@@ -1,36 +1,37 @@
-import { TypeOrmBuilder } from '../models/TypeOrmBuilder'
-import { Column, Table, TableBuilder } from '../models/table'
+import { Column, Table, TableBuilder } from "../models/table"
 
-let database: Table[] = JSON.parse(
-	JSON.stringify(require('../../assets/database.json'))
-)
+import { TypeOrmBuilder } from "../models/TypeOrmBuilder"
+import { SequelizeBuilder } from "../models/SequelizeBuilder"
 
-const getForeignKeys = (columns: Column[]): string[] => { 
-	return columns
-		.filter(({ references }) => references)
-		.map(({ references }) => references.id)
+let database: Table[] = JSON.parse(JSON.stringify(require("../../assets/database.json")))
+
+const getForeignKeys = (columns: Column[]): string[] => {
+	return columns.filter(({ references }) => references).map(({ references }) => references.id)
 }
 
-const getReferencedTables = (tables: Table[], foreignKeys: string[]): Table[] => { 
-	return tables.filter(({ columns }) => { 
+const getReferencedTables = (tables: Table[], foreignKeys: string[]): Table[] => {
+	return tables.filter(({ columns }) => {
 		return columns.some(({ id }) => foreignKeys.indexOf(id) >= 0)
 	})
 }
 
 const generateFiles = (tables: Table[], builder: TableBuilder) => {
-	tables.forEach(table => { 
-		if (!table.marked) { 
+	tables.forEach((table) => {
+		if (!table.marked) {
 			table.marked = true
 			let foreignKeys: string[] = getForeignKeys(table.columns)
 			let referencedTables: Table[] = getReferencedTables(tables, foreignKeys)
-			
+
 			generateFiles(referencedTables, builder)
-			
+
 			builder.buildTable(table)
 		}
 	})
 }
 
-let builder = new TypeOrmBuilder()
+let builder = new SequelizeBuilder()
 generateFiles(database, builder)
+
+builder.constructor.name === "SequelizeBuilder" && builder.buildConnection()
+
 builder.buildServer()
