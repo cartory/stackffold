f5758ffefc73da0eb73f6721e095ff814407e6ee
cartory <xXcartoryXx@gmail.com>
2021-12-22 17:31:47 -0400

    update user > add verifiedCode, isVerified

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,18 +1,78 @@
-import sequelize from '../sequelize'
-import { Model, DataTypes } from 'sequelize'
+import sequelize from "../sequelize"
+import { Model, DataTypes } from "sequelize"
 
-interface IUser {	id: number	uid: string	name: string	email: string	verifiedEmail: string	photoUrl: string	password: string	phoneNumber: string}
+interface IUser {
+	id: number
+	uid: string
+	name: string
+	email: string
+	verifiedEmail: string
+	photoUrl: string
+	password: string
+	phoneNumber: string
+	verifiedCode: string
+	isVerified: boolean
+}
 
-class User extends Model<IUser> { }
+class User extends Model<IUser> {}
 
-User.init({	id: {		key: 'id',		type: DataTypes.INTEGER({ length: 10 }),		primaryKey: true,		autoIncrement: true,		autoIncrementIdentity: true,	},
-	uid: {		key: 'uid',		type: DataTypes.STRING(50),		unique: true,	},
-	name: {		key: 'name',		type: DataTypes.STRING(50),	},
-	email: {		key: 'email',		type: DataTypes.STRING(50),		unique: true,	},
-	verifiedEmail: {		key: 'verifiedEmail',		type: DataTypes.STRING(50),		unique: true,	},
-	photoUrl: {		key: 'photoUrl',		type: DataTypes.STRING(255),	},
-	password: {		key: 'password',		type: DataTypes.STRING(255),	},
-	phoneNumber: {		key: 'phoneNumber',		type: DataTypes.STRING(20),	},
-}, {	sequelize, 	tableName: 'User', 	deletedAt: true,	timestamps: true, })
+User.init(
+	{
+		id: {
+			key: "id",
+			type: DataTypes.INTEGER({ length: 10 }),
+			primaryKey: true,
+			autoIncrement: true,
+			autoIncrementIdentity: true,
+		},
+		uid: {
+			key: "uid",
+			type: DataTypes.STRING(50),
+			unique: true,
+		},
+		name: {
+			key: "name",
+			type: DataTypes.STRING(50),
+		},
+		email: {
+			key: "email",
+			type: DataTypes.STRING(50),
+			unique: true,
+		},
+		verifiedEmail: {
+			key: "verifiedEmail",
+			type: DataTypes.STRING(50),
+			unique: true,
+		},
+		photoUrl: {
+			key: "photoUrl",
+			type: DataTypes.STRING(255),
+		},
+		password: {
+			key: "password",
+			type: DataTypes.STRING(255),
+		},
+		phoneNumber: {
+			key: "phoneNumber",
+			type: DataTypes.STRING(20),
+		},
+		verifiedCode: {
+			key: "verifiedCode",
+			type: DataTypes.STRING(4),
+		},
+
+		isVerified: {
+			key: "isVerified",
+			type: DataTypes.BOOLEAN,
+			defaultValue: false,
+		},
+	},
+	{
+		sequelize,
+		tableName: "User",
+		deletedAt: true,
+		timestamps: true,
+	}
+)
 
 export default User
