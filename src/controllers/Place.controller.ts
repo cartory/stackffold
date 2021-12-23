1841fc2eb38223707547bfabac7d590676b33a75
cartory <xXcartoryXx@gmail.com>
2021-12-23 11:17:45 -0400

    update place > findOne, findAll

--- src/controllers/Place.controller.ts
+++ src/controllers/Place.controller.ts
@@ -1,11 +1,38 @@
-import { Response } from 'express'
-import { Response as Res, Get, Post, Delete, Body, Params, Controller } from '@decorators/express'
+import { Response } from "express"
+import { Response as Res, Get, Post, Delete, Body, Params, Controller, Query } from "@decorators/express"
 
-import { IPlace } from '../models/Place'
-import { Place } from '../utils/models'
+import { IPlace } from "../models/Place"
+import { Place, Task, Type } from "../utils/models"
 
-@Controller('/places')export class PlaceController {
-	@Get('/')	async findAll(@Res() res: Response<Place[]>): Promise<Response<Place[]>> {		try {
+@Controller("/places")
+export class PlaceController {
+	@Get("/")
+	async findAll(@Query() query, @Res() res: Response<Place[]>): Promise<Response<Place[]>> {
+		const { page = 0, limit = 10 } = query
+		try {
+			const type = await Type.findOne({ order: [["id", "ASC"]] })
+
+			const places = await Place.findAll({
+				offset: page * limit,
+				limit: Number.parseInt(limit),
+				where: {
+					Typeid: type?._attributes.id,
+				},
+				include: [
+					{ model: Type, as: "type" },
+					{ model: Task, as: "tasks" },
+					{
+						model: Place,
+						association: "places",
+						through: { attributes: [] },
+						include: [
+							{ model: Type, as: "type" },
+							{ model: Task, as: "tasks" },
+						],
+					},
+				],
+			})
+
 			return res.status(200).json(await Place.findAll())
 		} catch (err) {
 			console.error(err)
@@ -13,15 +40,35 @@ import { Place } from '../utils/models'
 		return res.status(500).send([])
 	}
 
-	@Get('/:id')	async findOne(@Params('id') id: number, @Res() res: Response<Place>): Promise<Response<Place>> {		try {
-			return res.status(200).json(await Place.findOne({ where: { id } }))
+	@Get("/:id")
+	async findOne(@Params("id") id: number, @Res() res: Response<Place>): Promise<Response<Place>> {
+		try {
+			const place = await Place.findOne({
+				where: { id },
+				include: [
+					{ model: Type, as: "type" },
+					{ model: Task, as: "tasks" },
+					{
+						model: Place,
+						association: "places",
+						through: { attributes: [] },
+						include: [
+							{ model: Type, as: "type" },
+							{ model: Task, as: "tasks" },
+						],
+					},
+				],
+			})
+			return res.status(200).json(place)
 		} catch (err) {
 			console.error(err)
 		}
 		return res.status(500).send(null)
 	}
 
-	@Post('/')	async save(@Body() place: IPlace, @Res() res: Response<Place>): Promise<Response<Place>> {		try {
+	@Post("/")
+	async save(@Body() place: IPlace, @Res() res: Response<Place>): Promise<Response<Place>> {
+		try {
 			return res.status(201).json((await Place.upsert(place))[0])
 		} catch (err) {
 			console.error(err)
@@ -29,7 +76,9 @@ import { Place } from '../utils/models'
 		return res.status(500).send(null)
 	}
 
-	@Delete('/:id')	async destroy(@Params('id') id: number, @Res() res: Response): Promise<Response> {		try {
+	@Delete("/:id")
+	async destroy(@Params("id") id: number, @Res() res: Response): Promise<Response> {
+		try {
 			return res.status(202).send(await Place.destroy({ where: { id } }))
 		} catch (err) {
 			console.error(err)
