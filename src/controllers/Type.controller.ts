4e47036708ed1ddd23b1a6ca590bef0dab5d5998
cartory <xXcartoryXx@gmail.com>
2021-12-23 11:18:00 -0400

    update type > findAll, findOne

--- src/controllers/Type.controller.ts
+++ src/controllers/Type.controller.ts
@@ -1,27 +1,49 @@
-import { Response } from 'express'
-import { Response as Res, Get, Post, Delete, Body, Params, Controller } from '@decorators/express'
+import { Response } from "express"
+import { Response as Res, Get, Post, Delete, Body, Params, Controller } from "@decorators/express"
 
-import { IType } from '../models/Type'
-import { Type } from '../utils/models'
+import { IType } from "../models/Type"
+import { Place, Type } from "../utils/models"
 
-@Controller('/types')export class TypeController {
-	@Get('/')	async findAll(@Res() res: Response<Type[]>): Promise<Response<Type[]>> {		try {
-			return res.status(200).json(await Type.findAll())
+@Controller("/types")
+export class TypeController {
+	@Get("/")
+	async findAll(@Res() res: Response<Type[]>): Promise<Response<Type[]>> {
+		try {
+			return res.status(200).json(
+				await Type.findAll({
+					include: {
+						model: Place,
+						association: "places",
+					},
+				})
+			)
 		} catch (err) {
 			console.error(err)
 		}
 		return res.status(500).send([])
 	}
 
-	@Get('/:id')	async findOne(@Params('id') id: number, @Res() res: Response<Type>): Promise<Response<Type>> {		try {
-			return res.status(200).json(await Type.findOne({ where: { id } }))
+	@Get("/:id")
+	async findOne(@Params("id") id: number, @Res() res: Response<Type>): Promise<Response<Type>> {
+		try {
+			return res.status(200).json(
+				await Type.findOne({
+					where: { id },
+					include: {
+						model: Place,
+						association: "places",
+					},
+				})
+			)
 		} catch (err) {
 			console.error(err)
 		}
 		return res.status(500).send(null)
 	}
 
-	@Post('/')	async save(@Body() type: IType, @Res() res: Response<Type>): Promise<Response<Type>> {		try {
+	@Post("/")
+	async save(@Body() type: IType, @Res() res: Response<Type>): Promise<Response<Type>> {
+		try {
 			return res.status(201).json((await Type.upsert(type))[0])
 		} catch (err) {
 			console.error(err)
@@ -29,7 +51,9 @@ import { Type } from '../utils/models'
 		return res.status(500).send(null)
 	}
 
-	@Delete('/:id')	async destroy(@Params('id') id: number, @Res() res: Response): Promise<Response> {		try {
+	@Delete("/:id")
+	async destroy(@Params("id") id: number, @Res() res: Response): Promise<Response> {
+		try {
 			return res.status(202).send(await Type.destroy({ where: { id } }))
 		} catch (err) {
 			console.error(err)
