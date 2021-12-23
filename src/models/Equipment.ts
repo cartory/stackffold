168a2cf8114a9d5c3f19896331ee173e753ca2f4
cartory <xXcartoryXx@gmail.com>
2021-12-23 17:44:13 -0400

    update equipment

--- src/models/Equipment.ts
+++ src/models/Equipment.ts
@@ -1,65 +1,17 @@
-import sequelize from "../utils/sequelize"
-import { Model, DataTypes } from "sequelize"
+import sequelize from '../utils/sequelize'
+import { Model, DataTypes } from 'sequelize'
 
-export interface IEquipment {
-	id: number
-	description: string
-	code: string
-	photoUrl: string
-	state: string
-	observations: string
-	Unitid: number
-}
+export interface IEquipment {	id: number	description: string	code: string	photoUrl: string	state: string	observations: string	Unitid: number	Placeid: number}
 
-export class Equipment extends Model<IEquipment> {}
+export class Equipment extends Model<IEquipment> { }
 
-Equipment.init(
-	{
-		id: {
-			key: "id",
-			type: DataTypes.INTEGER({ length: 11 }),
-			primaryKey: true,
-			autoIncrement: true,
-			autoIncrementIdentity: true,
-		},
-		description: {
-			key: "description",
-			type: DataTypes.TEXT,
-		},
-		code: {
-			key: "code",
-			type: DataTypes.STRING(100),
-			unique: true,
-		},
-		photoUrl: {
-			key: "photoUrl",
-			type: DataTypes.STRING(255),
-			allowNull: true,
-		},
-		state: {
-			key: "state",
-			type: DataTypes.STRING(50),
-		},
-		observations: {
-			key: "observations",
-			type: DataTypes.TEXT,
-			allowNull: true,
-		},
-		Unitid: {
-			key: "Unitid",
-			type: DataTypes.INTEGER({ length: 11 }),
-			references: { key: "id", model: "Unit" },
-		},
-	},
-	{
-		sequelize,
-		tableName: "Equipment",
-		deletedAt: true,
-		timestamps: true,
-		defaultScope: {
-			attributes: {
-				exclude: ["Unitid"],
-			},
-		},
-	}
-)
+Equipment.init({	id: {		key: 'id',		type: DataTypes.INTEGER({ length: 11 }),		primaryKey: true,		autoIncrement: true,		autoIncrementIdentity: true,	},
+	description: {		key: 'description',		type: DataTypes.TEXT,	},
+	code: {		key: 'code',		type: DataTypes.STRING(100),		unique: true,	},
+	photoUrl: {		key: 'photoUrl',		type: DataTypes.STRING(255),		allowNull: true,	},
+	state: {		key: 'state',		type: DataTypes.STRING(50),	},
+	observations: {		key: 'observations',		type: DataTypes.TEXT,		allowNull: true,	},
+	Unitid: {		key: 'Unitid',		type: DataTypes.INTEGER({ length: 11 }),		references: { key: 'id', model: 'Unit' },	},
+	Placeid: {		key: 'Placeid',		type: DataTypes.INTEGER({ length: 10 }),		allowNull: true,		references: { key: 'id', model: 'Place' },	},
+}, {	sequelize, 	tableName: 'Equipment', 	deletedAt: true,	timestamps: true, })
+
