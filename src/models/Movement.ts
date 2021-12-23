153e28cad493e242b5f5ae45697069622d186a17
cartory <xXcartoryXx@gmail.com>
2021-12-23 17:43:27 -0400

    update movement > add userId

--- src/models/Movement.ts
+++ src/models/Movement.ts
@@ -1,7 +1,7 @@
 import sequelize from '../utils/sequelize'
 import { Model, DataTypes } from 'sequelize'
 
-export interface IMovement {	id: number	Reasonid: number	description: string	Equipmentid: number	placeFrom_id: number	placeTo_id: number}
+export interface IMovement {	id: number	Reasonid: number	description: string	Equipmentid: number	placeFrom_id: number	placeTo_id: number	Userid: number}
 
 export class Movement extends Model<IMovement> { }
 
@@ -11,5 +11,6 @@ Movement.init({	id: {		key: 'id',		type: DataTypes.INTEGER({ length: 11 }),	
 	Equipmentid: {		key: 'Equipmentid',		type: DataTypes.INTEGER({ length: 11 }),		references: { key: 'id', model: 'Equipment' },	},
 	placeFrom_id: {		key: 'placeFrom_id',		type: DataTypes.INTEGER({ length: 10 }),		allowNull: true,		references: { key: 'id', model: 'Place' },	},
 	placeTo_id: {		key: 'placeTo_id',		type: DataTypes.INTEGER({ length: 10 }),		allowNull: true,		references: { key: 'id', model: 'Place' },	},
+	Userid: {		key: 'Userid',		type: DataTypes.INTEGER({ length: 10 }),		references: { key: 'id', model: 'User' },	},
 }, {	sequelize, 	tableName: 'Movement', 	deletedAt: false,	timestamps: false, })
 
