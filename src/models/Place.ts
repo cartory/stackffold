9ed4fb447ecc0a8e19796df17e250b95ede34229
cartory <xXcartoryXx@gmail.com>
2021-12-23 12:11:40 -0400

    update place > exclude Typeid

--- src/models/Place.ts
+++ src/models/Place.ts
@@ -1,15 +1,60 @@
-import sequelize from '../utils/sequelize'
-import { Model, DataTypes } from 'sequelize'
+import sequelize from "../utils/sequelize"
+import { Model, DataTypes } from "sequelize"
 
-export interface IPlace {	id: number	code: string	name: string	description: string	Typeid: number	photoUrl: string}
+export interface IPlace {
+	id: number
+	code: string
+	name: string
+	description: string
+	Typeid: number
+	photoUrl: string
+}
 
-export class Place extends Model<IPlace> { }
+export class Place extends Model<IPlace> {}
 
-Place.init({	id: {		key: 'id',		type: DataTypes.INTEGER({ length: 10 }),		primaryKey: true,		autoIncrement: true,		autoIncrementIdentity: true,	},
-	code: {		key: 'code',		type: DataTypes.STRING(20),		unique: true,	},
-	name: {		key: 'name',		type: DataTypes.STRING(255),	},
-	description: {		key: 'description',		type: DataTypes.STRING(255),		allowNull: true,	},
-	Typeid: {		key: 'Typeid',		type: DataTypes.INTEGER({ length: 10 }),		references: { key: 'id', model: 'Type' },	},
-	photoUrl: {		key: 'photoUrl',		type: DataTypes.STRING(255),		allowNull: true,	},
-}, {	sequelize, 	tableName: 'Place', 	deletedAt: true,	timestamps: true, })
-
+Place.init(
+	{
+		id: {
+			key: "id",
+			type: DataTypes.INTEGER({ length: 10 }),
+			primaryKey: true,
+			autoIncrement: true,
+			autoIncrementIdentity: true,
+		},
+		code: {
+			key: "code",
+			type: DataTypes.STRING(20),
+			unique: true,
+		},
+		name: {
+			key: "name",
+			type: DataTypes.STRING(255),
+		},
+		description: {
+			key: "description",
+			type: DataTypes.STRING(255),
+			allowNull: true,
+		},
+		Typeid: {
+			key: "Typeid",
+			type: DataTypes.INTEGER({ length: 10 }),
+			references: { key: "id", model: "Type" },
+		},
+		photoUrl: {
+			key: "photoUrl",
+			type: DataTypes.STRING(255),
+			allowNull: true,
+		},
+	},
+	{
+		sequelize,
+		tableName: "Place",
+		deletedAt: true,
+		timestamps: true,
+		defaultScope: {
+			attributes: {
+				exclude: ["Typeid"],
+			},
+		},
+	}
+)
