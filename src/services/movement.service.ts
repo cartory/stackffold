deeca195ce176a41a531b0f9e2eef01b88e79c4c
cartory <xXcartoryXx@gmail.com>
2021-12-23 20:22:17 -0400

    update movement.service > add makeTransaction ☺️

--- src/services/movement.service.ts
+++ src/services/movement.service.ts
@@ -1,21 +1,43 @@
-import { Transaction } from "sequelize"
-import sequelize, { transaction } from "../utils/sequelize"
+import sequelize from "../utils/sequelize"
+import { QueryTypes, Transaction } from "sequelize"
 
-import { IMovement, Movement } from "../models/Movement"
+import { IMovement } from "../models/Movement"
 
-const makeTransaction = async (movement: IMovement, equipmentIDs: number[] = []) => {
-	const movements: IMovement[] = equipmentIDs.map((equipmentID) => {
-		return {
-			...movement,
-			Equipmentid: equipmentID,
-		} as IMovement
-	})
+/**
+ * Made with mySQL syntax
+ * This function make the transaction with one SQL query for performance
+ * @param movement is the movement for transaction
+ * @param ids are the number of equipment for transaction for movement
+ * @returns {Promise<boolean>} false if transaction fails
+ */
+const makeTransaction = async (movement: IMovement, ids: number[] = []): Promise<boolean> => {
+	if (!ids.length) {
+		return false
+	}
 
-	return await transaction(async (t: Transaction) => {
-		await Movement.bulkCreate(movements, { transaction: t })
-	})
+	const COLUMNS = Object.keys(movement).join(",")
+	const ROWS = ids.map((id) => `(${Object.values({ ...movement, Equipmentid: id } as IMovement).join(",")})`).join(",")
+
+	const sqlInsert = `INSERT INTO Movement (${COLUMNS}) VALUES ${ROWS}`
+	const sqlUpdate = `UPDATE Equipment SET Placeid = ${movement.placeTo_id} WHERE id IN (${ids.join(",")})`
+
+	try {
+		await sequelize.transaction((t: Transaction) => {
+			return Promise.all([
+				// INSERT && UPDATE
+				sequelize.query(sqlInsert, { transaction: t, type: QueryTypes.INSERT }),
+				sequelize.query(sqlUpdate, { transaction: t, type: QueryTypes.UPDATE }),
+			])
+		})
+
+		return true
+	} catch (err) {
+		console.error(err)
+	}
+
+	return false
 }
 
 export default {
-	makeTransaction
+	makeTransaction,
 }
