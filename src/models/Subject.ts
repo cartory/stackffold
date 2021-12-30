2c42629095e0bd833528f2958fc10611861c9035
cartory <xXcartoryXx@gmail.com>
2021-12-30 13:46:33 -0400

    update model > subject

--- src/models/Subject.ts
+++ src/models/Subject.ts
@@ -1,17 +1,16 @@
 import sequelize from '../utils/sequelize'
 import { Model, DataTypes } from 'sequelize'
 
-export interface ISubject {	id: number	code: string	name: string	HT: number	HP: number	HS: number	CR: number	Careerid: number}
+export interface ISubject {	id?: number	code: string	name: string	HT: number	HP: number	HS: number	CR: number}
 
 export class Subject extends Model<ISubject> { }
 
-Subject.init({	id: {		key: 'id',		type: DataTypes.INTEGER({ length: 11 }),		primaryKey: true,	},
+Subject.init({	id: {		key: 'id',		type: DataTypes.INTEGER({ length: 11 }),		primaryKey: true,		autoIncrement: true,		autoIncrementIdentity: true,	},
 	code: {		key: 'code',		type: DataTypes.STRING(10),		unique: true,	},
 	name: {		key: 'name',		type: DataTypes.STRING(100),	},
-	HT: {		key: 'HT',		type: DataTypes.TINYNIT,	},
-	HP: {		key: 'HP',		type: DataTypes.TINYNIT,	},
-	HS: {		key: 'HS',		type: DataTypes.TINYNIT,	},
-	CR: {		key: 'CR',		type: DataTypes.TINYNIT,	},
-	Careerid: {		key: 'Careerid',		type: DataTypes.INTEGER({ length: 11 }),		allowNull: true,		references: { key: 'id', model: 'Career' },	},
+	HT: {		key: 'HT',		type: DataTypes.INTEGER({ length: 5 }),	},
+	HP: {		key: 'HP',		type: DataTypes.INTEGER({ length: 5 }),	},
+	HS: {		key: 'HS',		type: DataTypes.INTEGER({ length: 5 }),	},
+	CR: {		key: 'CR',		type: DataTypes.INTEGER({ length: 5 }),	},
 }, {	sequelize, 	tableName: 'Subject', 	deletedAt: true,	timestamps: true, })
 
