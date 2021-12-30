32dc9f7fbc88178a6661dba0163e970f1e3c8817
cartory <xXcartoryXx@gmail.com>
2021-12-30 10:16:38 -0400

    add placeType, movementReason

---
+++ src/models/MovementReason.ts
@@ -0,0 +1,11 @@
+import sequelize from '../utils/sequelize'
+import { Model, DataTypes } from 'sequelize'
+
+export interface IMovementReason {	id?: number	name: string}
+
+export class MovementReason extends Model<IMovementReason> { }
+
+MovementReason.init({	id: {		key: 'id',		type: DataTypes.INTEGER({ length: 11 }),		primaryKey: true,		autoIncrement: true,		autoIncrementIdentity: true,	},
+	name: {		key: 'name',		type: DataTypes.STRING(20),		unique: true,	},
+}, {	sequelize, 	tableName: 'MovementReason', 	deletedAt: true,	timestamps: true, })
+
---
+++ src/models/PlaceType.ts
@@ -0,0 +1,11 @@
+import sequelize from '../utils/sequelize'
+import { Model, DataTypes } from 'sequelize'
+
+export interface IPlaceType {	id?: number	name: string}
+
+export class PlaceType extends Model<IPlaceType> { }
+
+PlaceType.init({	id: {		key: 'id',		type: DataTypes.INTEGER({ length: 10 }),		primaryKey: true,		autoIncrement: true,		autoIncrementIdentity: true,	},
+	name: {		key: 'name',		type: DataTypes.STRING(50),		unique: true,	},
+}, {	sequelize, 	tableName: 'PlaceType', 	deletedAt: true,	timestamps: true, })
+
