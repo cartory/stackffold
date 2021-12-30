563e045d3a0a7dd48e0a0ba2b64e88a242602b91
cartory <xXcartoryXx@gmail.com>
2021-12-30 13:46:02 -0400

    update requirement

--- src/models/Requirement.ts
+++ src/models/Requirement.ts
@@ -1,13 +1,33 @@
 import sequelize from '../utils/sequelize'
 import { Model, DataTypes } from 'sequelize'
 
-export interface IRequirement {	subjectReqId: number	subjectPreqId: number	Careerid: number	semester: number}
+export interface IRequirement {
+	subjectReqId: number
+	subjectPreqId: number
+	Careerid: number
+}
 
 export class Requirement extends Model<IRequirement> { }
 
-Requirement.init({	subjectReqId: {		key: 'subjectReqId',		type: DataTypes.INTEGER({ length: 11 }),		primaryKey: true,		references: { key: 'id', model: 'Subject' },	},
-	subjectPreqId: {		key: 'subjectPreqId',		type: DataTypes.INTEGER({ length: 11 }),		primaryKey: true,		references: { key: 'id', model: 'Subject' },	},
-	Careerid: {		key: 'Careerid',		type: DataTypes.INTEGER({ length: 11 }),		references: { key: 'id', model: 'Career' },	},
-	semester: {		key: 'semester',		type: DataTypes.TINYNIT,	},
-}, {	sequelize, 	tableName: 'Requirement', 	deletedAt: false,	timestamps: false, })
-
+Requirement.init({
+	subjectReqId: {
+		key: 'subjectReqId',
+		type: DataTypes.INTEGER({ length: 11 }),
+		references: { key: 'id', model: 'Subject' },
+	},
+	subjectPreqId: {
+		key: 'subjectPreqId',
+		type: DataTypes.INTEGER({ length: 11 }),
+		references: { key: 'id', model: 'Subject' },
+	},
+	Careerid: {
+		key: 'Careerid',
+		type: DataTypes.INTEGER({ length: 11 }),
+		references: { key: 'id', model: 'Career' },
+	},
+}, {
+	sequelize, 
+	tableName: 'Requirement', 
+	deletedAt: false,
+	timestamps: false, 
+})
