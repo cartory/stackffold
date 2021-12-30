3fbca035404bf817214574a57fc72a4922d13365
cartory <xXcartoryXx@gmail.com>
2021-12-30 13:41:43 -0400

    update router

--- src/router.ts
+++ src/router.ts
@@ -1,30 +1,34 @@
 import { Router } from 'express'
 import { attachControllers } from '@decorators/express'
 
-import { TypeController } from './controllers/Type.controller'
 import { UserController } from './controllers/User.controller'
 import { TaskController } from './controllers/Task.controller'
-import { UnitController } from './controllers/Unit.controller'
 import { PlaceController } from './controllers/Place.controller'
-import { ReasonController } from './controllers/Reason.controller'
 import { CareerController } from './controllers/Career.controller'
 import { SubjectController } from './controllers/Subject.controller'
 import { JobTitleController } from './controllers/JobTitle.controller'
+import { PlaceTypeController } from './controllers/PlaceType.controller'
 import { EquipmentController } from './controllers/Equipment.controller'
+import { EquipmentUnitController } from './controllers/EquipmentUnit.controller'
+import { EquipmentTypeController } from './controllers/EquipmentType.controller'
+import { EquipmentBrandController } from './controllers/EquipmentBrand.controller'
+import { MovementReasonController } from './controllers/MovementReason.controller'
 
 const router = Router()
 
 attachControllers(router, [
-	TypeController,
 	UserController,
 	TaskController,
-	UnitController,
 	PlaceController,
-	ReasonController,
 	CareerController,
 	SubjectController,
 	JobTitleController,
+	PlaceTypeController,
 	EquipmentController,
+	EquipmentUnitController,
+	EquipmentTypeController,
+	EquipmentBrandController,
+	MovementReasonController,
 ])
 
 export default router
